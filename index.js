73c52d7a025f35584da1770cf1a1d5d0b91bec15
Francis Gomes Leal <C1297721@interno.bb.com.br>
2019-06-14 17:07:50 -0300

    criacao do status bar

--- index.js
+++ index.js
@@ -3,14 +3,19 @@
  // AppRegistry.registerComponent(appName, () => Login); 
  */
 import React, { Component } from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry, StatusBar, View } from 'react-native';
 import Autonomia from './src/screens/Autonomia';
 import CustoTrajeto from './src/screens/CustoTrajeto';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 class Index extends Component {
     render() {
-        return (<Autonomia />);
+        return (
+            <View style={{ flex: 1, }}>
+                <StatusBar backgroundColor = 'black' />
+                <CustoTrajeto />
+            </View>
+        );
     }
 }
 AppRegistry.registerComponent(appName, () => Index);
